55f1ead7085b964244abc61bc1015d9d60279aa4
Kehan.Xue <kehan.xue@gmail.com>
2020-01-14 17:17:13 +0800

    First finish CeleX5Options class

--- src/bean/CeleX5Options.cpp
+++ src/bean/CeleX5Options.cpp
@@ -3,3 +3,109 @@
 //
 
 #include "CeleX5Options.h"
+
+CeleX5Options::CeleX5Options(CeleX5::CeleX5Mode fixed_mode,
+                             bool is_loop_mode_enabled,
+                             std::vector<CeleX5::CeleX5Mode> loop_modes,
+                             uint32_t event_duration,
+                             uint32_t threshold,
+                             uint32_t brightness,
+                             uint32_t contrast,
+                             uint32_t clock_rate,
+                             std::string event_fpn_file_path,
+                             std::string frame_fpn_file_path)
+    : fixed_mode_(fixed_mode),
+      is_loop_mode_enabled_(is_loop_mode_enabled),
+      loop_modes_(std::move(loop_modes)),
+      event_duration_(event_duration),
+      threshold_(threshold),
+      brightness_(brightness),
+      contrast_(contrast),
+      clock_rate_(clock_rate),
+      event_FPN_file_path_(std::move(event_fpn_file_path)),
+      frame_FPN_file_path_(std::move(frame_fpn_file_path)) {
+
+}
+
+CeleX5Options::~CeleX5Options() = default;
+
+CeleX5::CeleX5Mode CeleX5Options::GetFixedMode() const {
+  return fixed_mode_;
+}
+
+void CeleX5Options::SetFixedMode(CeleX5::CeleX5Mode fixed_mode) {
+  fixed_mode_ = fixed_mode;
+}
+
+bool CeleX5Options::IsLoopModeEnabled() const {
+  return is_loop_mode_enabled_;
+}
+
+void CeleX5Options::SetIsLoopModeEnabled(bool is_loop_mode_enabled) {
+  is_loop_mode_enabled_ = is_loop_mode_enabled;
+}
+
+const std::vector<CeleX5::CeleX5Mode> &CeleX5Options::GetLoopModes() const {
+  return loop_modes_;
+}
+
+void CeleX5Options::SetLoopModes(const std::vector<CeleX5::CeleX5Mode> &loop_modes) {
+  loop_modes_ = loop_modes;
+}
+
+uint32_t CeleX5Options::GetEventDuration() const {
+  return event_duration_;
+}
+
+void CeleX5Options::SetEventDuration(uint32_t event_duration) {
+  event_duration_ = event_duration;
+}
+
+uint32_t CeleX5Options::GetThreshold() const {
+  return threshold_;
+}
+
+void CeleX5Options::SetThreshold(uint32_t threshold) {
+  threshold_ = threshold;
+}
+
+uint32_t CeleX5Options::GetBrightness() const {
+  return brightness_;
+}
+
+void CeleX5Options::SetBrightness(uint32_t brightness) {
+  brightness_ = brightness;
+}
+
+uint32_t CeleX5Options::GetContrast() const {
+  return contrast_;
+}
+
+void CeleX5Options::SetContrast(uint32_t contrast) {
+  contrast_ = contrast;
+}
+
+uint32_t CeleX5Options::GetClockRate() const {
+  return clock_rate_;
+}
+
+void CeleX5Options::SetClockRate(uint32_t clock_rate) {
+  clock_rate_ = clock_rate;
+}
+
+const std::string &CeleX5Options::GetEventFpnFilePath() const {
+  return event_FPN_file_path_;
+}
+
+void CeleX5Options::SetEventFpnFilePath(const std::string &event_fpn_file_path) {
+  event_FPN_file_path_ = event_fpn_file_path;
+}
+
+const std::string &CeleX5Options::GetFrameFpnFilePath() const {
+  return frame_FPN_file_path_;
+}
+
+void CeleX5Options::SetFrameFpnFilePath(const std::string &frame_fpn_file_path) {
+  frame_FPN_file_path_ = frame_fpn_file_path;
+}
+
--- src/bean/CeleX5Options.h
+++ src/bean/CeleX5Options.h
@@ -13,12 +13,60 @@ class CeleX5Options {
 
  public:
 
-  static bool setCeleX5Options(CeleX5Options cele_x_5_options);
+  explicit CeleX5Options(CeleX5::CeleX5Mode fixed_mode = CeleX5::CeleX5Mode::Event_Off_Pixel_Timestamp_Mode,
+                         bool is_loop_mode_enabled = false,
+                         std::vector<CeleX5::CeleX5Mode> loop_modes = std::vector<CeleX5::CeleX5Mode>(3),
+                         uint32_t event_duration = 100,
+                         uint32_t threshold = 185,
+                         uint32_t brightness = 150,
+                         uint32_t contrast = 1,
+                         uint32_t clock_rate = 100,
+                         std::string event_fpn_file_path = "",
+                         std::string frame_fpn_file_path = "");
 
-// protected:
+  virtual ~CeleX5Options();
+
+  CeleX5::CeleX5Mode GetFixedMode() const;
+
+  void SetFixedMode(CeleX5::CeleX5Mode fixed_mode);
+
+  bool IsLoopModeEnabled() const;
+
+  void SetIsLoopModeEnabled(bool is_loop_mode_enabled);
+
+  const std::vector<CeleX5::CeleX5Mode> &GetLoopModes() const;
+
+  void SetLoopModes(const std::vector<CeleX5::CeleX5Mode> &loop_modes);
+
+  uint32_t GetEventDuration() const;
+
+  void SetEventDuration(uint32_t event_duration);
+
+  uint32_t GetThreshold() const;
+
+  void SetThreshold(uint32_t threshold);
+
+  uint32_t GetBrightness() const;
+
+  void SetBrightness(uint32_t brightness);
+
+  uint32_t GetContrast() const;
+
+  void SetContrast(uint32_t contrast);
+
+  uint32_t GetClockRate() const;
+
+  void SetClockRate(uint32_t clock_rate);
+
+  const std::string &GetEventFpnFilePath() const;
+
+  void SetEventFpnFilePath(const std::string &event_fpn_file_path);
+
+  const std::string &GetFrameFpnFilePath() const;
+
+  void SetFrameFpnFilePath(const std::string &frame_fpn_file_path);
 
  private:
-
   CeleX5::CeleX5Mode fixed_mode_;
 
   bool is_loop_mode_enabled_;
