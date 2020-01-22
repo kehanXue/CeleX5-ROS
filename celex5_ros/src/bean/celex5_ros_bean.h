edf44c3627b5f8d37a6498d8df8624fe881a544c
Kehan.Xue <kehan.xue@gmail.com>
2020-01-22 13:17:32 +0800

    Add LICENSE.

--- celex5_ros/src/bean/celex5_ros_bean.h
+++ celex5_ros/src/bean/celex5_ros_bean.h
@@ -1,6 +1,21 @@
-//
-// Created by kehan on 2020/1/18.
-//
+/**
+ *  The ROS package for CeleX^TM CeleX5-MIPI Dynamic Vision Sensor.
+ *
+ *  Copyright (C) 2020  Kehan.Xue<kehan.xue@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef CELEX5_ROS_SRC_BEAN_CELEX5ROSBEAN_H_
 #define CELEX5_ROS_SRC_BEAN_CELEX5ROSBEAN_H_
