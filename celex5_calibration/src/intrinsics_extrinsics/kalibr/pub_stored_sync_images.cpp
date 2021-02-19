b09096f02a1b341faf8e137cdf740608a10955d5
Kehan Xue <kehan.xue@gmail.com>
2021-02-19 14:46:48 +0800

    Fix bugs with the new ddynamic_configure version.

--- celex5_calibration/src/intrinsics_extrinsics/kalibr/pub_stored_sync_images.cpp
+++ celex5_calibration/src/intrinsics_extrinsics/kalibr/pub_stored_sync_images.cpp
@@ -21,76 +21,95 @@
 
 #include "pub_stored_sync_images.h"
 
+
 PubStoredSyncImages::PubStoredSyncImages(const ros::NodeHandle &nh, bool is_publish_enable, int publish_rate)
-    : nh_(nh),
-      is_publish_enable_(is_publish_enable),
-      publish_rate_(publish_rate) {
+        : nh_(nh),
+          is_publish_enable_(is_publish_enable),
+          publish_rate_(publish_rate)
+{
 
-  std::string camera1_dir("camera1");
-  nh_.param("camera1_dir", camera1_dir, camera1_dir);
-  std::string camera1_pattern = camera1_dir + "*.jpg";
-  cv::glob(camera1_pattern, image1_filenames_, false);
+    std::string camera1_dir("camera1");
+    nh_.param("camera1_dir", camera1_dir, camera1_dir);
+    std::string camera1_pattern = camera1_dir + "*.jpg";
+    cv::glob(camera1_pattern, image1_filenames_, false);
 
-  std::string camera2_dir("camera2");
-  nh_.param("camera2_dir", camera2_dir, camera2_dir);
-  std::string camera2_pattern = camera2_dir + "*.jpg";
-  cv::glob(camera2_pattern, image2_filenames_, false);
+    std::string camera2_dir("camera2");
+    nh_.param("camera2_dir", camera2_dir, camera2_dir);
+    std::string camera2_pattern = camera2_dir + "*.jpg";
+    cv::glob(camera2_pattern, image2_filenames_, false);
 
-  std::string image1_topic_name("camera1/raw_image");
-  nh_.param("image1_topic_name", image1_topic_name, image1_topic_name);
-  image1_pub_ = nh_.advertise<sensor_msgs::Image>(image1_topic_name, 1);
+    std::string image1_topic_name("camera1/raw_image");
+    nh_.param("image1_topic_name", image1_topic_name, image1_topic_name);
+    image1_pub_ = nh_.advertise<sensor_msgs::Image>(image1_topic_name, 1);
 
-  std::string image2_topic_name("camera2/raw_image");
-  nh_.param("image2_topic_name", image2_topic_name, image2_topic_name);
-  image2_pub_ = nh_.advertise<sensor_msgs::Image>(image2_topic_name, 1);
+    std::string image2_topic_name("camera2/raw_image");
+    nh_.param("image2_topic_name", image2_topic_name, image2_topic_name);
+    image2_pub_ = nh_.advertise<sensor_msgs::Image>(image2_topic_name, 1);
 
-  p_ddyn_rec_ = std::make_shared<ddynamic_reconfigure::DDynamicReconfigure>(nh_);
-  p_ddyn_rec_->registerVariable("is_publish_enable", &is_publish_enable_, "Publish enable.");
+    p_ddyn_rec_ = std::make_shared<ddynamic_reconfigure::DDynamicReconfigure>(nh_);
+    p_ddyn_rec_->registerVariable<bool>("is_publish_enable", &is_publish_enable_, "Publish enable.", false, true);
 
-  p_main_thread_ = std::make_shared<std::thread>([&]() {
-    ros::Rate loop_rate(publish_rate_);
-    p_ddyn_rec_->registerVariable<int>("publish_rate", publish_rate_,
-                                       [&loop_rate](int new_fps) {
-                                         loop_rate = ros::Rate(new_fps);
-                                       }, "Publish rate. Unit hz.", 0, 200);
+    p_main_thread_ = std::make_shared<std::thread>([&]()
+                                                   {
+                                                       ros::Rate loop_rate(publish_rate_);
+                                                       p_ddyn_rec_->registerVariable<int>("publish_rate", publish_rate_,
+                                                                                          [&loop_rate](int new_fps)
+                                                                                          {
+                                                                                              loop_rate = ros::Rate(
+                                                                                                      new_fps);
+                                                                                          }, "Publish rate. Unit hz.",
+                                                                                          0, 200);
 
-    if (image1_filenames_.size() != image1_filenames_.size()) {
-      ROS_ERROR("The each number of sync images set doesn't equal!");
-      return;
-    }
+                                                       if (image1_filenames_.size() != image1_filenames_.size())
+                                                       {
+                                                           ROS_ERROR(
+                                                                   "The each number of sync images set doesn't equal!");
+                                                           return;
+                                                       }
 
-    int index = 0;
-    ros::Time now_stamp = ros::Time::now();
-    auto read_file_and_publish =
-        [&index, &now_stamp](const ros::Publisher &pub, const std::vector<cv::String> &image_filenames) {
-          cv::Mat image = cv::imread(image_filenames.at(index));
-          sensor_msgs::ImagePtr image_ptr_msg =
-              cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
-          image_ptr_msg->header.stamp = now_stamp; // TODO add frame_id
-          pub.publish(image_ptr_msg);
-        };
+                                                       int index = 0;
+                                                       ros::Time now_stamp = ros::Time::now();
+                                                       auto read_file_and_publish =
+                                                               [&index, &now_stamp](const ros::Publisher &pub,
+                                                                                    const std::vector<cv::String> &image_filenames)
+                                                               {
+                                                                   cv::Mat image = cv::imread(
+                                                                           image_filenames.at(index));
+                                                                   sensor_msgs::ImagePtr image_ptr_msg =
+                                                                           cv_bridge::CvImage(std_msgs::Header(),
+                                                                                              "bgr8",
+                                                                                              image).toImageMsg();
+                                                                   image_ptr_msg->header.stamp = now_stamp; // TODO add frame_id
+                                                                   pub.publish(image_ptr_msg);
+                                                               };
 
-    while (ros::ok()) {
-      if (is_publish_enable_) {
-        if (index >= image1_filenames_.size()) {
-          ROS_INFO("Publish all images finished.");
-          break;
-        }
-        now_stamp = ros::Time::now();
-        read_file_and_publish(image1_pub_, image1_filenames_);
-        read_file_and_publish(image2_pub_, image2_filenames_);
-        index++;
-      }
-      loop_rate.sleep();
-    }
-  });
+                                                       while (ros::ok())
+                                                       {
+                                                           if (is_publish_enable_)
+                                                           {
+                                                               if (index >= image1_filenames_.size())
+                                                               {
+                                                                   ROS_INFO("Publish all images finished.");
+                                                                   break;
+                                                               }
+                                                               now_stamp = ros::Time::now();
+                                                               read_file_and_publish(image1_pub_, image1_filenames_);
+                                                               read_file_and_publish(image2_pub_, image2_filenames_);
+                                                               index++;
+                                                           }
+                                                           loop_rate.sleep();
+                                                       }
+                                                   });
 
-  usleep(3000);
-  p_ddyn_rec_->publishServicesTopics();
+    usleep(3000);
+    p_ddyn_rec_->publishServicesTopics();
 }
 
-PubStoredSyncImages::~PubStoredSyncImages() {
-  if (p_main_thread_->joinable()) {
-    p_main_thread_->join();
-  }
+
+PubStoredSyncImages::~PubStoredSyncImages()
+{
+    if (p_main_thread_->joinable())
+    {
+        p_main_thread_->join();
+    }
 }
--- celex5_calibration/src/intrinsics_extrinsics/kalibr/sync_images.cpp
+++ celex5_calibration/src/intrinsics_extrinsics/kalibr/sync_images.cpp
@@ -21,82 +21,90 @@
 
 #include "sync_images.h"
 
+
 SyncImages::SyncImages(const ros::NodeHandle &nh)
-    : nh_(nh),
-      store_dir_("./"),
-      save_(false) {
+        : nh_(nh),
+          store_dir_("./"),
+          save_(false)
+{
 
-  /*
-   * Create a new clean directory to store images
-   */
-  nh_.param("store_dir", store_dir_, store_dir_);
-  // TODO Fix bugs, doesn't work.
-  // if (!boost::filesystem::is_empty(store_dir_)) {
-  //   boost::filesystem::remove_all(store_dir_);
-  // }
-  // if (!boost::filesystem::exists(store_dir_)) {
-  //   boost::filesystem::create_directory(store_dir_);
-  // boost::filesystem::create_directory(store_dir_ + "frame/");
-  // boost::filesystem::create_directory(store_dir_ + "events/");
-  // }
+    /*
+     * Create a new clean directory to store images
+     */
+    nh_.param("store_dir", store_dir_, store_dir_);
+    // TODO Fix bugs, doesn't work.
+    // if (!boost::filesystem::is_empty(store_dir_)) {
+    //   boost::filesystem::remove_all(store_dir_);
+    // }
+    // if (!boost::filesystem::exists(store_dir_)) {
+    //   boost::filesystem::create_directory(store_dir_);
+    // boost::filesystem::create_directory(store_dir_ + "frame/");
+    // boost::filesystem::create_directory(store_dir_ + "events/");
+    // }
 
-  /*
-   * Create sync images handle
-   */
-  std::string frame_topic_name("/frame_image");
-  nh_.param("frame_topic", frame_topic_name, frame_topic_name);
-  p_frame_sync_sub_ = std::make_shared<MfImageSub>(nh_, frame_topic_name, 2,
-                                                   ros::TransportHints().tcpNoDelay());
-  std::string events_topic_name("/events_image");
-  nh_.param("events_topic", events_topic_name, events_topic_name);
-  p_events_sync_sub_ = std::make_shared<MfImageSub>(nh_, events_topic_name, 2,
-                                                    ros::TransportHints().tcpNoDelay());
+    /*
+     * Create sync images handle
+     */
+    std::string frame_topic_name("/frame_image");
+    nh_.param("frame_topic", frame_topic_name, frame_topic_name);
+    p_frame_sync_sub_ = std::make_shared<MfImageSub>(nh_, frame_topic_name, 2,
+                                                     ros::TransportHints().tcpNoDelay());
+    std::string events_topic_name("/events_image");
+    nh_.param("events_topic", events_topic_name, events_topic_name);
+    p_events_sync_sub_ = std::make_shared<MfImageSub>(nh_, events_topic_name, 2,
+                                                      ros::TransportHints().tcpNoDelay());
 
-  p_sync_ = std::make_shared<message_filters::Synchronizer<SyncPolicy>>(
-      SyncPolicy(10), *p_frame_sync_sub_, *p_events_sync_sub_
-  );
-  p_sync_->registerCallback(boost::bind(&SyncImages::SyncImagesCallback, this, _1, _2));
+    p_sync_ = std::make_shared<message_filters::Synchronizer<SyncPolicy>>(
+            SyncPolicy(10), *p_frame_sync_sub_, *p_events_sync_sub_
+    );
+    p_sync_->registerCallback(boost::bind(&SyncImages::SyncImagesCallback, this, _1, _2));
 
-  p_ddyn_rec_ = std::make_shared<ddynamic_reconfigure::DDynamicReconfigure>(nh_);
-  p_ddyn_rec_->registerVariable<bool>("save", &save_, "Save images");
-  p_ddyn_rec_->publishServicesTopics();
-  // frame_sub_ = nh_.subscribe<sensor_msgs::Image>(frame_topic_name, 10,
-  //                                                &SyncImages::FrameImageCallback,
-  //                                                this);
-  // events_sub_ = nh_.subscribe<sensor_msgs::Image>(events_topic_name, 10,
-  //                                                 &SyncImages::EventsImageCallback,
-  //                                                 this);
+    p_ddyn_rec_ = std::make_shared<ddynamic_reconfigure::DDynamicReconfigure>(nh_);
+    p_ddyn_rec_->registerVariable<bool>("save", &save_, "Save images", false, true);
+    p_ddyn_rec_->publishServicesTopics();
+    // frame_sub_ = nh_.subscribe<sensor_msgs::Image>(frame_topic_name, 10,
+    //                                                &SyncImages::FrameImageCallback,
+    //                                                this);
+    // events_sub_ = nh_.subscribe<sensor_msgs::Image>(events_topic_name, 10,
+    //                                                 &SyncImages::EventsImageCallback,
+    //                                                 this);
 }
+
 
 SyncImages::~SyncImages() = default;
 
+
 void SyncImages::SyncImagesCallback(const sensor_msgs::ImageConstPtr &frame_msg,
-                                    const sensor_msgs::ImageConstPtr &events_msg) {
-  if (save_) {
-    cv_bridge::CvImagePtr frame_image_ptr;
-    cv_bridge::CvImagePtr events_image_ptr;
-    try {
-      // TODO Gray or RGB
-      frame_image_ptr = cv_bridge::toCvCopy(frame_msg, sensor_msgs::image_encodings::BGR8);
-      events_image_ptr = cv_bridge::toCvCopy(events_msg, sensor_msgs::image_encodings::MONO8);
+                                    const sensor_msgs::ImageConstPtr &events_msg)
+{
+    if (save_)
+    {
+        cv_bridge::CvImagePtr frame_image_ptr;
+        cv_bridge::CvImagePtr events_image_ptr;
+        try
+        {
+            // TODO Gray or RGB
+            frame_image_ptr = cv_bridge::toCvCopy(frame_msg, sensor_msgs::image_encodings::BGR8);
+            events_image_ptr = cv_bridge::toCvCopy(events_msg, sensor_msgs::image_encodings::MONO8);
+        }
+        catch (cv_bridge::Exception &e)
+        {
+            ROS_ERROR_STREAM("cv_bridge Exception:" << e.what());
+            return;
+        }
+
+        cv::Mat frame_img = frame_image_ptr->image;
+        cv::Mat events_img = events_image_ptr->image;
+
+        std::string pair_id = std::to_string(frame_msg->header.seq);
+        // boost::filesystem::create_directory(store_dir_ + pair_id);
+
+        // ROS_INFO("store_path, %s", store_dir.c_str());
+        cv::imwrite(store_dir_ + "frame/" + pair_id + ".jpg", frame_img);
+        cv::imwrite(store_dir_ + "events/" + pair_id + ".jpg", events_img);
+        cv::waitKey(1);
+        ROS_INFO("Write! %s", pair_id.c_str());
     }
-    catch (cv_bridge::Exception &e) {
-      ROS_ERROR_STREAM("cv_bridge Exception:" << e.what());
-      return;
-    }
-
-    cv::Mat frame_img = frame_image_ptr->image;
-    cv::Mat events_img = events_image_ptr->image;
-
-    std::string pair_id = std::to_string(frame_msg->header.seq);
-    // boost::filesystem::create_directory(store_dir_ + pair_id);
-
-    // ROS_INFO("store_path, %s", store_dir.c_str());
-    cv::imwrite(store_dir_ + "frame/" + pair_id + ".jpg", frame_img);
-    cv::imwrite(store_dir_ + "events/" + pair_id + ".jpg", events_img);
-    cv::waitKey(1);
-    ROS_INFO("Write! %s", pair_id.c_str());
-  }
 }
 
 // void SyncImages::FrameImageCallback(const sensor_msgs::ImageConstPtr &frame_msg) {
